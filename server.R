b094d3cfd585ae9051980ebcf3bf464c6ed9cb2a
PedroMilanezAlmeida <pedromilanez@gmail.com>
2020-01-09 14:51:33 -0300

    add files

---
+++ server.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+shinyServer(function(input, output) {
+    seatbelts <- as.data.frame(Seatbelts)
+    seatbelts <- mutate(seatbelts,
+                        kmsNorm = (kms - mean(kms))/1000)
+    
+    coefStats <- reactive({
+        set.seed(2019)
+        if(input$sliderSampleSize > 0){
+        propInput <- input$sliderSampleSize
+        seatbeltsProp <- seatbelts[sample(dim(seatbelts)[1],
+                                          size = (dim(seatbelts)[1]*(propInput/100)) %>%
+                                              as.integer(.)),]
+        coefStatsRes <- glm(formula = DriversKilled ~ kmsNorm, 
+                            family = "poisson",
+                            data = seatbeltsProp) %>%
+            summary(.) %>%
+            .$coef
+        coefStatsRes <- ifelse(abs(coefStatsRes) < 1e-3,
+                               yes = formatC(coefStatsRes,
+                                             format = "e",
+                                             digits = 1),
+                               no = round(coefStatsRes,3))
+        rownames(coefStatsRes) <- c("intercept", "distance driven")
+        } else {
+            coefStatsRes <- "Please pick a value > 0"
+        }
+        return(coefStatsRes)
+    })
+    
+    output$plot1 <- renderPlot({
+        set.seed(2019)
+        propInput <- input$sliderSampleSize
+        seatbeltsProp <- seatbelts[sample(dim(seatbelts)[1],
+                                          size = (dim(seatbelts)[1]*(propInput/100)) %>%
+                                              as.integer(.)),]
+        
+        ggplot(data = seatbeltsProp,
+               aes(kmsNorm,
+                   DriversKilled)) +
+            geom_point() +
+            geom_smooth(method = "glm",
+                        method.args = list(family = 'poisson')) +
+            theme_bw() +
+            xlab("distance driven (centered; 1000km)") +
+            ylab("drivers killed/month") +
+            xlim(c(-7.4,6.7)) +
+            ylim(c(59,199))
+
+    })
+    
+    output$coefStatsRes <- renderTable({
+        coefStats()
+    },
+    rownames = TRUE)
+    
+})
---
+++ ui.R
@@ -0,0 +1,41 @@
+library(shiny)
+shinyUI(fluidPage(
+    titlePanel("Model Uncertainty with Varying Sample Sizes"),
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("sliderSampleSize",
+                        "What proportion of samples to use?",
+                        min = 0,
+                        max = 100, 
+                        post  = " %",
+                        value = 100),
+            submitButton("Submit")
+        ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel(
+                    "Main",
+                    plotOutput("plot1"),
+                    h3("Coefficient Statistics:"),
+                    tableOutput("coefStatsRes")
+                    ),
+                tabPanel(
+                    "Documentation",
+                    h4("Slide button"),
+                    helpText("Slide the button to choose the percentage of samples
+                             to use in regression."),
+                    br(),
+                    h4("Model fit"),
+                    helpText("Under the hood, a linear model is fit to the log of the
+                             mean (glm(y~x, family = poisson)). Please take this into
+                             consideration when interpreting the coefficients."),
+                    br(),
+                    h4("Plot"),
+                    helpText("In blue is the fitted line, in gray is the 95% confidence
+                             interval of the mean."),
+                    br(),
+                    h4("Data Source"),
+                    helpText("Seatbelts in the datasets package. Please type ?Seatbelts
+                             in R for details."))
+            )))
+))
